62d3cf9f89435103a75b1c5d729e1aa8cf40945a
Cristian Garzon <desarrollo.cristian.garzon@gmail.com>
2023-04-27 16:11:00 -0500

    agregando visat de detalles del usuario

--- src/app/modules/user/user-detail/user-detail.component.ts
+++ src/app/modules/user/user-detail/user-detail.component.ts
@@ -1,18 +1,22 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
+import { USERS_DATA } from '@data/constants/users.const';
+import { ICardUser } from '@shared/components/cards/card-user/icard-user.metadata';
 
 @Component({
   selector: 'app-user-detail',
   templateUrl: './user-detail.component.html',
   styleUrls: ['./user-detail.component.scss']
 })
-export class UserDetailComponent {
+export class UserDetailComponent{
   public id: number;
-
+  public users: ICardUser[] = USERS_DATA;
+  public currentrUsers: ICardUser | null;
   constructor(
       private route: ActivatedRoute
     ){
-      this.id = this.route.snapshot.params['id'];
+      this.id = +this.route.snapshot.params['id'];
+      this.currentrUsers = this.users.find((user) => user.id === this.id ) || null;
     }
 
 
