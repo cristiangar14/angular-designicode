a3e1637bdd40565ee3d688b4bdfab196f263d016
Cristian Garzon <desarrollo.cristian.garzon@gmail.com>
2023-04-27 10:52:28 -0500

    Agregando el componente de H1

--- src/app/shared/components/index.ts
+++ src/app/shared/components/index.ts
@@ -1,8 +1,9 @@
 // Import components
-
+import { TitleH1Component } from './title-h1/title-h1.component'
 
 export const components: any[] = [
-
+  TitleH1Component
 ];
 
 //Export all components
+export * from './title-h1/title-h1.component'
