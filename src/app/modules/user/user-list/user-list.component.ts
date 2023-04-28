cbb9f57468e93bca7db53fb38f0fd502f78521a4
Cristian Garzon <desarrollo.cristian.garzon@gmail.com>
2023-04-28 11:29:19 -0500

    Implementando el carruserl en la vista de lisat de usuarios

--- src/app/modules/user/user-list/user-list.component.ts
+++ src/app/modules/user/user-list/user-list.component.ts
@@ -1,8 +1,10 @@
 import { Component, OnInit } from '@angular/core';
+import { CAROUSEL_DATA_ITEMS } from '@data/constants/carousel.const';
 import { USERS_DATA } from '@data/constants/users.const';
 import { UserService } from '@data/services/api/user.service';
 
 import { ICardUser } from '@shared/components/cards/card-user/icard-user.metadata';
+import { ICarouselItem } from '@shared/components/carousel/ICarousel-item.metadata';
 
 @Component({
   selector: 'app-user-list',
@@ -10,6 +12,7 @@ import { ICardUser } from '@shared/components/cards/card-user/icard-user.metadat
   styleUrls: ['./user-list.component.scss']
 })
 export class UserListComponent{
+  public carouselData: ICarouselItem[] = CAROUSEL_DATA_ITEMS;
   public users: ICardUser[] | null = null;
   public text: string = 'Lista de usuarios';
   public type: string =  'primary';
