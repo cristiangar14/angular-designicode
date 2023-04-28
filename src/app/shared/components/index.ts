3b493c274f916b2997a5172113193c9d92c45dd6
Cristian Garzon <desarrollo.cristian.garzon@gmail.com>
2023-04-28 11:38:13 -0500

    iniciando componente de card-loader

--- src/app/shared/components/index.ts
+++ src/app/shared/components/index.ts
@@ -2,9 +2,11 @@
 import { TitleH1Component } from './titles/title-h1/title-h1.component';
 import { CardUserComponent } from './cards/card-user/card-user.component';
 import { CarouselComponent } from './carousel/carousel.component';
+import { CardLoaderComponent } from './loaders/card-loader/card-loader.component';
 
 export const components: any[] = [
   CarouselComponent,
+  CardLoaderComponent,
   TitleH1Component,
   CardUserComponent
 ];
@@ -13,3 +15,4 @@ export const components: any[] = [
 export * from './titles/title-h1/title-h1.component';
 export * from './cards/card-user/card-user.component';
 export * from './carousel/carousel.component';
+export * from './loaders/card-loader/card-loader.component';
--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -8,6 +8,7 @@ import { TitleH1Component } from './components/titles/title-h1/title-h1.componen
 import { CardUserComponent } from './components/cards/card-user/card-user.component';
 import { RouterModule } from '@angular/router';
 import { CarouselComponent } from './components/carousel/carousel.component';
+import { CardLoaderComponent } from './components/loaders/card-loader/card-loader.component';
 
 @NgModule({
   imports: [
