85066501b371ab0a278c67289652374a4df94e2c
Cristian Garzon <desarrollo.cristian.garzon@gmail.com>
2023-04-28 11:26:31 -0500

    Agregando información mock para el carrusel

---
+++ src/app/data/constants/carousel.const.ts
@@ -0,0 +1,45 @@
+import { ICarouselItem } from "@shared/components/carousel/ICarousel-item.metadata";
+
+export const CAROUSEL_DATA_ITEMS: ICarouselItem[] = [
+  {
+    id: 1,
+    title: {
+      first: 'TITULO',
+      second: 'uno'
+    },
+    subtitle: 'Esto es una gran descripción',
+    link: '/',
+    image: 'https://img.freepik.com/vector-gratis/letras-verano-foto_23-2148523749.jpg?size=626&ext=jpg'
+  },
+  {
+    id: 2,
+    title: {
+      first: 'TITULO',
+      second: 'dos'
+    },
+    subtitle: 'Esto es una gran descripción',
+    link: '/',
+    image: 'https://img.freepik.com/foto-gratis/cuento-fantasia-infantil-planetas-espacio_23-2150165803.jpg?size=626&ext=jpg'
+  },
+  {
+    id: 3,
+    title: {
+      first: 'TITULO',
+      second: 'tres'
+    },
+    subtitle: 'Esto es una gran descripción',
+    link: '/',
+    image: 'https://img.freepik.com/foto-gratis/tecnologia-primer-plano-placa-base-procesador-computadora-placa-circuito-microchip-integrado-luz-neon_90220-1203.jpg?size=626&ext=jpg'
+  },
+  {
+    id: 4,
+    title: {
+      first: 'TITULO',
+      second: 'cuatro'
+    },
+    subtitle: 'Esto es una gran descripción',
+    link: '/',
+    image: 'https://img.freepik.com/fotos-premium/taza-te-sentada-mesa-vistas-montana-fondo-ai-generativo_192745-1330.jpg?size=626&ext=jpg'
+  },
+
+]
